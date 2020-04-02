2bcfc685e8dba9f4450f25a7953bb32742aa8732
lizarel@163.com <lizarel@163.com>
2020-04-02 19:30:26 +0800

    fix;

--- wstmart/api/controller/Users.php
+++ wstmart/api/controller/Users.php
@@ -44,4 +44,41 @@ class Users extends Base
         }
        return $this->outJson(0, "认证成功");
    }
+
+    /*
+     * 更新用户信息
+     */
+    public function updateMember()
+    {
+        $user_id = $this->request->param("user_id", '', "intval");
+        $nick_name = $this->request->post("nick_name", '');
+        $avatar = $this->request->post("avatar", '');
+        $sex = $this->request->post("sex", '', "intval");
+        if (mb_strlen($nick_name,"utf-8") > 8) {
+            $this->outJson(1, "昵称限制8个字符！");
+        }
+        $member = \wstmart\api\model\Users::where("userId", $user_id)->find();
+        if (empty($member)) {
+            return $this->outJson(1, "没有数据！");
+        }
+        $member->userName = empty($nick_name) ? $member->userName : $nick_name;
+        $member->userPhoto = empty($avatar) ? $member->userPhoto : $avatar;
+        $member->userSex = empty($sex) ? $member->userSex : $sex;
+        $member->save();
+        return $this->outJson(0, "保存成功！");
+    }
+
+    /*
+     * 用户信息详情
+     */
+    public function memberDetail()
+    {
+        $user_id = $this->request->param("user_id", '', "intval");
+        $member = \wstmart\api\model\Users::where("userId", $user_id)->field("userName, userSex, userPhoto, userPhone")->find();
+        if (empty($member)) {
+            return $this->outJson(1, "指定的用户不存在！");
+        }
+        return $this->outJson(0, "查找成功！", $member);
+    }
+
 }
